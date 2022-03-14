e91dc25c0700a979bd77c24e78c6c7ae289913b2
Stephen Beattie <beattie19@hotmail.co.uk>
2022-03-14 22:04:28 +1100

    :sparkles: Add dead letter queue

--- sqs.tf
+++ sqs.tf
@@ -1,9 +1,16 @@
 resource "aws_sqs_queue" "populatePokemon" {
   name                      = "populate-pokemon"
-  delay_seconds             = 90
   max_message_size          = 2048
   message_retention_seconds = 86400
   receive_wait_time_seconds = 10
+    redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.dead_letter_queue.arn
+    maxReceiveCount     = 1
+  })
+}
+
+resource "aws_sqs_queue" "dead_letter_queue" {
+  name = "populate-pokemon-DLQ"
 }
 
 resource "aws_sqs_queue_policy" "sqs_send" {
