80a576a5b6373999509236f62fe752ee104519ff
Stephen Beattie <beattie19@hotmail.co.uk>
2022-04-12 23:35:17 +1000

    :adhesive_bandage: Update outputs to fix extra forward slash

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "populate_url" {
-  value = "${aws_api_gateway_deployment.pokemon_deployment.invoke_url}/${var.environment}/${aws_api_gateway_resource.populate-pokemon-resource.path_part}"
+  value = "${aws_api_gateway_deployment.pokemon_deployment.invoke_url}${var.environment}/${aws_api_gateway_resource.populate-pokemon-resource.path_part}"
 }
 
 output "all_pokemon_url" {
-  value = "${aws_api_gateway_deployment.pokemon_deployment.invoke_url}/${var.environment}/${aws_api_gateway_resource.all-pokemon-resource.path_part}"
+  value = "${aws_api_gateway_deployment.pokemon_deployment.invoke_url}${var.environment}/${aws_api_gateway_resource.all-pokemon-resource.path_part}"
 }
 
 output "custom_populate_url" {
